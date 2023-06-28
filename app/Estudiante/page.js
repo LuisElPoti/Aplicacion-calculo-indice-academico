78de76fed817a5b1320dc7cc112ab65d4eba81f0
Allen Silverio <allensilverioo>
2023-06-28 05:41:45 -0400

    layout estudiante listo

---
+++ app/Estudiante/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContenedorIndicadores from '../components/ContenedorIndicadores'
+import ContenedorIndiceGeneral from '../components/ContenedorIndiceGeneral'
+import ContenedorInformacion from '../components/ContenedorInformacion'
+
+export default function MenuPrincipalEstudiante() {
+  return (
+    <div>page</div>
+  )
+}
