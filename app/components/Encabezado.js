2d145ea83128716dd5bf8796900fb962a96433ca
Allen Silverio <allensilverioo>
2023-06-28 17:20:11 -0400

    Menu Principal

--- app/components/Encabezado.js
+++ app/components/Encabezado.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function Encabezado() {
   return (
     <div className="encabezado">
@@ -8,8 +9,8 @@ function Encabezado() {
           <div className="paola-salda-a">Paola Saldaña</div>
           <div className="estudiante">Estudiante</div>
         </div>
-        <img className="foto-perfil" alt="Foto perfil" src="images/foto-perfil.svg" />
-        <img className="chevron-down" alt="Chevron down" src="images/chevron-down.svg" />
+        <img className="foto-perfil" alt="Foto perfil" src="/images/foto-perfil.svg" />
+        <img className="chevron-down" alt="Chevron down" src="/images/chevron-down.svg" />
       </div>
       <div className="titulo-fecha">
         <div className="men-principal">Menú Principal</div>
--- app/components/Logo.js
+++ app/components/Logo.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export const Logo = ({ style }) => {
   return (
     <div className="logo" style={style}>
-      <img className="img" alt="Logo" src="images/Logo.svg" />
+      <img className="img" alt="Logo" src="/images/Logo.svg" />
     </div>
   );
 };
