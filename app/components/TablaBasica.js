19dfc01194aa3c1a1fddded5a7f42b87b57da564
Paola <saldanaquezada@gmail.com>
2023-06-28 08:28:32 -0400

    --

---
+++ app/components/TablaBasica.js
@@ -0,0 +1,52 @@
+"use client"
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(Asignatura, Seccion, Aula, Horario, Profesor) {
+  return { Asignatura, Seccion, Aula, Horario, Profesor };
+}
+
+const rows = [
+  createData('Estructuras de Datos - IDS305', 1, 'GC-303', 'LU-MI 14/16', 'Allen Silverio'),
+  createData('Estructuras de Datos - IDS305', 2, 'GC-303', 'LU-MI 16/18', 'Allen Silverio'),
+];
+
+function TablaBasica() {
+  return (
+    <TableContainer component={Paper} sx={{ width: 700 , fontfamily: 'Poppins' }}>
+      <Table sx={{ width: 700, height: 400}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Asignatura</TableCell>
+            <TableCell align="right">Seccion</TableCell>
+            <TableCell align="right">Aula</TableCell>
+            <TableCell align="right">Horario</TableCell>
+            <TableCell align="right">Profesor</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="right">{row.Asignatura}</TableCell>
+              <TableCell align="right">{row.Seccion}</TableCell>
+              <TableCell align="right">{row.Aula}</TableCell>
+              <TableCell align="right">{row.Horario}</TableCell>
+              <TableCell align="right">{row.Profesor}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default TablaBasica;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-
+import TablaBasica from './components/TablaBasica';
 
 
  function page() {
