415462d04c63a1bb3023cb2ad4bc42f572017660
Allen Silverio <allensilverioo>
2023-06-28 01:50:34 -0400

    Diseño Menú Principal

--- app/page.js
+++ app/page.js
@@ -1,22 +1,17 @@
-"use client";
+import MenuPrincipal from "./components/MenuPrincipal"
 import Link from 'next/link';
-import Encabezado from './components/Encabezado';
-import ContenedorInformacion from './components/ContenedorInformacion';
-import ContenedorIndiceGeneral from './components/ContenedorIndiceGeneral';
-import ContenedorIndicadores from './components/ContenedorIndicadores';
+
 
 
  function page() {
 
 
   return (
-<>
-    <Encabezado />
-    <ContenedorInformacion />
-    <ContenedorIndiceGeneral />
-    <ContenedorIndicadores />
-</>
-
+    <>
+      <MenuPrincipal/>
+      
+    </>
+    
 
 
   
