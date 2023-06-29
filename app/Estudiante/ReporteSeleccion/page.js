ecd580d1ea564b94957866a921617cb925a83f60
Paola <saldanaquezada@gmail.com>
2023-06-29 12:41:49 -0400

    cambios tabla

--- app/Estudiante/ReporteSeleccion/page.js
+++ app/Estudiante/ReporteSeleccion/page.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import FilreiReporteSeleccion from '@/app/components/FiltroReporteSeleccion'
+import FiltroReporteSeleccion from '@/app/components/FiltroReporteSeleccion'
 import TablaBasica from '@/app/components/TablaBasica'
 import BotonGuardar from '@/app/components/BotonGuardar'
 
  function ReporteSeleccion() {
+
+  const headers = ['Asignatura', 'Seccion', 'Aula', 'Horario', 'Profesor'];
+
+  const data = [
+    { Asignatura: 'Estructuras de Datos - IDS305', Seccion: 1, Aula: 'GC-303', Horario: 'LU-MI 14/16', Profesor: 'Allen Silverio' },
+    { Asignatura: 'Estructuras de Datos - IDS305', Seccion: 2, Aula: 'GC-303', Horario: 'LU-MI 16/18', Profesor: 'Allen Silverio' },
+  ];
+
   return (
 
 <>
-    <FilreiReporteSeleccion/>
-    <TablaBasica/>
+    <FiltroReporteSeleccion/>
+    <TablaBasica headers={headers} data={data}/>
     <BotonGuardar/>
-
 </>
 
 
