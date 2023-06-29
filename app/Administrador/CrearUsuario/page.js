e8232c5254f3fefb1fbc1daf84e52762e2f2f736
Allen Silverio <allensilverioo>
2023-06-29 09:02:53 -0400

    Tabla Componente greedy

--- app/Administrador/CrearUsuario/page.js
+++ app/Administrador/CrearUsuario/page.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import MenuPrincipalAdmin from '../components/MenuPrincipalAdmin'
+
 
 export default function CrearUsuario() {
   return (
-    <MenuPrincipalAdmin/>
+    <>Crear Usuario</>
+    
   )
 }
--- app/components/TablaBasica.js
+++ app/components/TablaBasica.js
@@ -7,46 +7,43 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+/*
 function createData(Asignatura, Seccion, Aula, Horario, Profesor) {
   return { Asignatura, Seccion, Aula, Horario, Profesor };
 }
-
 const rows = [
   createData('Estructuras de Datos - IDS305', 1, 'GC-303', 'LU-MI 14/16', 'Allen Silverio'),
   createData('Estructuras de Datos - IDS305', 2, 'GC-303', 'LU-MI 16/18', 'Allen Silverio'),
 ];
+*/
 
-function TablaBasica() {
+const TablaBasica = ({ headers, data }) => {
   return (
-    <TableContainer component={Paper} sx={{ width: 730 , fontfamily: 'Poppins', height:400 }}>
-      <Table sx={{ width: 700, height: 100}} aria-label="simple table">
+    <TableContainer component={Paper} sx={{ width: 730, fontFamily: 'Poppins', height: 400 }}>
+      <Table sx={{ width: 700, height: 100 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Asignatura</TableCell>
-            <TableCell align="right">Seccion</TableCell>
-            <TableCell align="right">Aula</TableCell>
-            <TableCell align="right">Horario</TableCell>
-            <TableCell align="right">Profesor</TableCell>
+            {headers.map((header, index) => (
+              <TableCell align="center" key={index}>
+                {header}
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell align="right">{row.Asignatura}</TableCell>
-              <TableCell align="right">{row.Seccion}</TableCell>
-              <TableCell align="right">{row.Aula}</TableCell>
-              <TableCell align="right">{row.Horario}</TableCell>
-              <TableCell align="right">{row.Profesor}</TableCell>
+          {data.map((row, rowIndex) => (
+            <TableRow key={rowIndex} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              {Object.values(row).map((value, columnIndex) => (
+                <TableCell align="right" key={columnIndex}>
+                  {value}
+                </TableCell>
+              ))}
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
   );
-}
+};
 
 export default TablaBasica;
