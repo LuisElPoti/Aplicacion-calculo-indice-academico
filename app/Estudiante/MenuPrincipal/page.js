1e0681f9ce8cc44273ddf37c35831047ce05b580
Paola <saldanaquezada@gmail.com>
2023-06-29 15:46:29 -0400

    ---

--- app/Estudiante/MenuPrincipal/page.js
+++ app/Estudiante/MenuPrincipal/page.js
@@ -5,6 +5,15 @@ import ContenedorInformacion from '../../components/ContenedorInformacion'
 import TablaBasica from '../../components/TablaBasica'
 
 export default function MenuPrincipalEstudiante() {
+
+
+    const headers = ['Asignatura', 'Seccion', 'Aula', 'Horario', 'Profesor'];
+
+    const data = [
+      { Asignatura: 'Estructuras de Datos - IDS305', Seccion: 1, Aula: 'GC-303', Horario: 'LU-MI 14/16', Profesor: 'Allen Silverio' },
+      { Asignatura: 'Estructuras de Datos - IDS305', Seccion: 2, Aula: 'GC-303', Horario: 'LU-MI 16/18', Profesor: 'Allen Silverio' },
+    ];
+
     return (
        
         <div className= 'grid grid-cols-3 gap-3'>
@@ -12,7 +21,7 @@ export default function MenuPrincipalEstudiante() {
             <ContenedorIndicadores />
             <ContenedorIndiceGeneral />
             <div className='row-span-2 ml-2'><ContenedorInformacion /></div>
-            <div className='col-span-2'><TablaBasica /></div>
+            <TablaBasica headers={headers} data={data}/>
             
         </div>
 
