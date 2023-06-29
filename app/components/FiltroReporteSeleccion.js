1c27205ae3226d251720a0da3e7e3acdc50467e8
Allen Silverio <allensilverioo>
2023-06-29 09:48:36 -0400

    Pagina Filtro Reporte Seleccion

--- app/components/FiltroReporteSeleccion.js
+++ app/components/FiltroReporteSeleccion.js
@@ -16,25 +16,7 @@ export default function SelectLabels() {
 
   return (
     <div>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-          
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <FormHelperText>With label + helper text</FormHelperText>
-      </FormControl>
+
       <FormControl sx={{ m: 1, minWidth: 120 }}>
         <Select
           value={age}
