38579476e36d80282dbd2ff08c8a7b49b54405c1
pazzispb <pazzispb20@gmail.com>
2023-06-30 05:26:09 -0400

    Cambios en la interfaz de seleccion, favor validar

--- app/Estudiante/ReporteSeleccion/page.js
+++ app/Estudiante/ReporteSeleccion/page.js
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import React, {useState} from 'react';
 import Cookies from 'js-cookie';
 import FiltroReporteSeleccion from '@/app/components/FiltroReportes'
 import TablaBasica from '@/app/components/TablaBasica'
@@ -25,56 +25,82 @@ const headers = ['Asignatura', 'Seccion', 'Aula', 'Horario', 'Profesor'];
 
 const id_usuario = Cookies.get('id_usuario');
 
-const requestData = {
-  id: id_usuario,
-  a_o: 2023,
-  trimestre: 1,
+const [año, setAño] = useState('');
+const [trimestre, setTrimestre] = useState('');
+
+const [data, setData] = useState([]);
+
+const handleClick = async () => {
+  const newData = await getSeleccion(id_usuario, año, trimestre);
+  setData(newData)
 };
 
-const response = await fetch('http://localhost:3000/api/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(requestData),
-  });
+const handleOnChangeTrim = (event) => {
+  setTrimestre(event.target.value)
+  console.log(event.target.value)
+};
 
-  const historico_academico = await response.json();
 
-  const data = historico_academico.map((historico) => {
-  const asignatura = historico.secciones?.asignaturas?.nombre || 'Prueba';
-  const seccion = historico.secciones?.numero || 'Prueba';
-  var aula = ''
-  var horario = ''; 
+const handleOnChangeAño = (event) => {
+  setAño(event.target.value)
+  console.log(event.target.value)
+};
 
-  for (let i = 0; i < historico.secciones.horario_secciones.length; i++) {
-    aula += historico.secciones.horario_secciones[i].aula + '  ';
-    horario += historico.secciones.horario_secciones[i].hora_inicio + '/' + historico.secciones.horario_secciones[i].hora_fin + '  ';
-  }
-  
-  const profesor = historico.secciones?.profesores?.nombre + " " + historico.secciones?.profesores?.apellido || 'Prueba';
-
-  return {
-    Asignatura: asignatura,
-    Seccion: seccion,
-    Aula: aula,
-    Horario: horario,
-    Profesor: profesor,
-  };
-});
 
 return (
 
 <>
 <div className='flex'>
-<FiltroReporteSeleccion items={menuItemsYear} label="Año"/> 
-<FiltroReporteSeleccion items={menuItemsTrimestre} label="Trimestre" /> 
+<FiltroReporteSeleccion items={menuItemsYear} label="Año" onChange={handleOnChangeAño}/> 
+<FiltroReporteSeleccion items={menuItemsTrimestre} label="Trimestre" onChange={handleOnChangeTrim} /> 
 </div>
   <TablaBasica headers={headers} data={data}/>
   <BotonGuardar/>
 </>
 
 )
+}
+
+async function getSeleccion(id_usuario, año, trimestre){
+  const requestData = {
+    id: id_usuario,
+    a_o: año,
+    trimestre: trimestre,
+  };
+  
+  const response = await fetch('http://localhost:3000/api/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    });
+  
+    const historico_academico = await response.json();
+  
+    const data = historico_academico.map((historico) => {
+    const asignatura = historico.secciones?.asignaturas?.nombre || 'Prueba';
+    const seccion = historico.secciones?.numero || 'Prueba';
+    var aula = ''
+    var horario = ''; 
+  
+    for (let i = 0; i < historico.secciones.horario_secciones.length; i++) {
+      aula += historico.secciones.horario_secciones[i].aula + '  ';
+      horario += historico.secciones.horario_secciones[i].hora_inicio + '/' + historico.secciones.horario_secciones[i].hora_fin + '  ';
+    }
+    
+    const profesor = historico.secciones?.profesores?.nombre + " " + historico.secciones?.profesores?.apellido || 'Prueba';
+  
+    return {
+      Asignatura: asignatura,
+      Seccion: seccion,
+      Aula: aula,
+      Horario: horario,
+      Profesor: profesor,
+    };
+  });
+  return data;
+  
 }
 
 export default ReporteSeleccion
