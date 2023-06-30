a453b4c45127ad4254737442a191befd5267347a
Allen Silverio <allensilverioo>
2023-06-29 21:15:24 -0400

    arreglo tarjeta calificaciones

--- app/components/Tarjeta.js
+++ app/components/Tarjeta.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
 
-export default function Tarjeta({ headerValues, className }) {
+export default function Tarjeta({ headerValues, className, imageSource }) {
   return (
-    <div className={`tarjeta flex pl-7 ${className}`}>
-      <Image src="/images/cuate.svg" width={130} height={275} />
-      <div className='content pl-5 mt-2'>
+    <div className={`tarjeta flex pl-7 ${className} pb-3`}>
+      <Image src={imageSource} className='mt-2' width={100} height={275} />
+      <div className='content ml-8'>
         {headerValues.map((header) => (
-          <div className='tarjeta-value flex pt-1' key={header.headerName}>
+          <div className='tarjeta-value flex mt-3' key={header.headerName}>
             <h6 className='mr-3 font-bold tarjeta-header'>{header.headerName}:</h6>
-            <p className='tarjeta-value pb-2'>{header.headerValue}</p>
+            <p className='tarjeta-value'>{header.headerValue}</p>
           </div>
         ))}
       </div>
