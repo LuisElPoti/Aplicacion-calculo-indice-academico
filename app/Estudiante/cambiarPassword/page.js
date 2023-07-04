ff3a92cf0bab7fe668b85a30c8fce1434ec29147
Allen Silverio <allensilverioo>
2023-07-03 21:42:09 -0400

    arreglo cambiar password interface

--- app/Estudiante/cambiarPassword/page.js
+++ app/Estudiante/cambiarPassword/page.js
@@ -6,14 +6,9 @@ export default function CambiarPassword() {
     <div className="cambiar-contrasea">
       <div className="overlap-group-wrapper">
         <div className="overlap-group">
-        <Image src="/images/amico.svg" className="education-amico" width={100} height={100} />
+        <Image src="/images/amico.svg" className="education-amico" width={50} height={50} />
           <CambiarPasswordForm
             logoLogo="logo-2.png"
-            style={{
-              left: "0",
-              position: "absolute",
-              top: "113px",
-            }}
           />
         </div>
       </div>
