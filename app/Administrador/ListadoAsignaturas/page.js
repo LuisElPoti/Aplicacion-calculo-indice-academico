f8bf39968fd7c35dd4504470b925418c9b8b494d
pazzispb <pazzispb20@gmail.com>
2023-07-07 04:41:19 -0400

    Filtro implementado en listado de asignatura

--- app/Administrador/ListadoAsignaturas/page.js
+++ app/Administrador/ListadoAsignaturas/page.js
@@ -1,36 +1,78 @@
 "use client"
-import React, { useState } from 'react';
-
-import TablaBasica from '../../components/TablaBasica'
-import { TextField } from '@mui/material';
-import BotonGuardar from '@/app/components/BotonGuardar';
-
-
-const headers = ['Clave', 'Asignatura', 'Creditos', 'Area'];
-
-const data = [
-  { Clave: 'IDS305', Asignatura: 'Estructuras de Datos', Creditos: '01', Area: 'Ingenierias'},
-  { Clave: 'IDS305', Asignatura: 'Estructuras de Datos', Creditos: '01', Area: 'Ingenierias'},];
+import React, { useEffect, useState } from 'react';
+import TableComponent from "@/app/components/TableFilterAsignaturas"
+import { CiTrash, CiPen } from "react-icons/ci";
+import axios from 'axios';
 
 function ListadoAsignaturas() {
+  const [data, setData] = useState([]);
+  const headers = ['Clave', 'Asignatura', 'Creditos', 'Area'];
 
-  const [searchText, setSearchText] = useState('');
+  const handleEditClick = (event, id_usuario, rol) => {
+    // setID(id_usuario);
+    // setRol(rol);
+    // setShowEditContainer(!showEditContainer);
+  };
 
+  const handleDeleteClick = async (event, id_usuario, rol) => {
+    // setID(id_usuario);
+    // setRol(rol);
+    // const result = confirm(`¿Está seguro que desea inhabilitar al usuario ${id_usuario}?`, "Esta acción no se puede deshacer");
+    // if (result) {
+    //   const requestData = {
+    //     id_usuario: id_usuario,
+    //     tipo_usuario: rol.toLowerCase(),
+    //   };
+    //   const response = await axios.post(
+    //     "../api/Usuarios/EliminarUsuario",
+    //     requestData,
+    //     {
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //     });
+    //   if (response.status === 200) {
+    //     alert(`Usuario inhabilitado con éxito`);
+
+    //   } else {
+    //     console.log("Problema");
+    //     alert('Hubo problemas al inhabilitar el nuevo usuario, inténtelo de nuevo');
+    //   }
+    // } else {
+    //   console.log("Cancelled.");
+    // }
+
+  };
+
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get("../api/Asignatura")
+        .then(response => {
+          const newData = response.data.map(obj => {
+            return {
+              Clave: obj.clave,
+              Asignatura: obj.nombre,
+              Creditos: obj.creditos,
+              Area: obj.areas_academicas.nombre,
+              Opciones: <button onClick={(e) => handleDeleteClick(e, obj.Id, obj.Tipo)}><CiTrash style={{ width: "50px", height: "25px", color: '#DE5462' }} /></button>,
+              Editar: <button onClick={(e) => handleEditClick(e, obj.Id, obj.Tipo)}><CiPen style={{ width: "50px", height: "25px", color: 'gray' }} /></button>
+            };
+          });
+          setData(newData);
+
+        })
+        .catch(error => {
+          console.error("Error al obtener a los usuarios:", error);
+        });
+    };
+    fetchData();
+  }, []);
   return (
     <>
-        <div className='filters flex items-center m-5 l-0'>
-        <TextField
-          label="Search"
-          placeholder="Search by Asignatura, Seccion, Aula"
-          value={searchText}
-          onChange={e => setSearchText(e.target.value)}
-          sx={{width: '50%', backgroundColor: '#fff',  outline:'none', borderRadius: '20px'}}
-        />
-        <BotonGuardar texto='Cargar Asignaturas' className={'amarillo ml-10'} onClick={" "}/>
-    </div >
-    <div classname = "flex items-center t-30">
-      <TablaBasica headers={headers} data={data} />
-    </div>
+      <div classname="flex items-center t-30">
+        <TableComponent headers={headers} data={data} />
+      </div>
     </>
 
   );
--- app/api/Asignatura/route.js
+++ app/api/Asignatura/route.js
@@ -6,11 +6,24 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET(req){
-    try { 
-        const asignatura = await prisma.asignaturas.findMany();
+export async function GET(req) {
+    try {
+        const asignatura = await prisma.asignaturas.findMany(
+            {
+                select: {
+                    clave: true,
+                    nombre: true,
+                    creditos: true,
+                    areas_academicas: {
+                        select: {
+                            nombre: true
+                        }
+                    }
+                }
+            }
+        );
         console.log(asignatura)
-        return NextResponse.json( asignatura , { status: 200 }); // 200 OK
+        return NextResponse.json(asignatura, { status: 200 }); // 200 OK
 
     } catch (error) {
         console.error('Error al obtener las asignaturas:', error); // 500 Internal Server Error
