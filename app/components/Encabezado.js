d299d62b166112cb8e84982a0e4687393d9d4683
Allen Silverio <allensilverioo>
2023-07-14 11:57:52 -0400

    arreglo menu de administrador

--- app/components/Encabezado.js
+++ app/components/Encabezado.js
@@ -135,6 +135,14 @@ function Encabezado({ userName }) {
           role = 'Administrador'
           break;
 
+          case '/Administrador/ListadoSecciones':
+          menuTitle = 'Crear nueva seccion';
+          color = 'color-admin'; // replace with actual color value
+          role = 'Administrador'
+          break;
+
+
+
       // add more cases as needed...
       default:
         menuTitle = 'Root';
